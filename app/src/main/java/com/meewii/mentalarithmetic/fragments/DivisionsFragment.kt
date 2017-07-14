068223cf0c59039d01f123fe9ed5744c0eee14cb
Marie-Anne Sinfreu <ma.sinfreu@gmail.com>
2017-07-14 16:52:40 +0200

    Create app with fragment navigation

---
+++ app/src/main/java/com/meewii/mentalarithmetic/fragments/DivisionsFragment.kt
@@ -0,0 +1,24 @@
+package com.meewii.mentalarithmetic.fragments
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.meewii.mentalarithmetic.R
+import kotlinx.android.synthetic.main.fragment_operation.*
+
+class DivisionsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_operation, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        currentFormula.setText(R.string.divisions)
+    }
+
+}
