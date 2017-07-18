dad746f140591ec39c42a1077f882be532a9b7b0
Marie-Anne Sinfreu <ma.sinfreu@gmail.com>
2017-07-18 18:06:16 +0200

    [wip] RecyclerView adapter for the past formulas

---
+++ app/src/main/java/com/meewii/mentalarithmetic/MainAdapter.kt
@@ -0,0 +1,34 @@
+package com.meewii.mentalarithmetic
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.meewii.mentalarithmetic.models.Operation
+import kotlinx.android.synthetic.main.list_item_past_formula.view.*
+
+
+class MainAdapter(operations: MutableList<Operation>) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+
+    var mOperations: MutableList<Operation> = operations
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(layoutInflater.inflate(R.layout.list_item_past_formula, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val operation = mOperations[position]
+        val pastFormula : String = operation.formula + " = " + operation.solution
+        holder.pastFormula.text = pastFormula
+    }
+
+    override fun getItemCount(): Int {
+        return mOperations.size
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val pastFormula = view.pastFormula!!
+    }
+
+}
--- app/src/main/java/com/meewii/mentalarithmetic/fragments/AdditionsFragment.kt
+++ app/src/main/java/com/meewii/mentalarithmetic/fragments/AdditionsFragment.kt
@@ -5,10 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.meewii.mentalarithmetic.MainAdapter
 import com.meewii.mentalarithmetic.R
+import com.meewii.mentalarithmetic.models.Operation
+import com.meewii.mentalarithmetic.models.OperationType
 import kotlinx.android.synthetic.main.fragment_additions.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
+import java.util.*
 
 class AdditionsFragment : Fragment() {
+
+    private var myAdapter: MainAdapter? = null
+    private var mutableList: MutableList<Operation>? = null
+    private var layoutManager: RecyclerView.LayoutManager? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_additions, container, false)
@@ -17,8 +28,40 @@ class AdditionsFragment : Fragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         currentFormula.setText(R.string.additions)
+
+        initialize()
+        setupList()
+        setUpCalculator()
+//        loadData()
+    }
+
+    private fun initialize() {
+        mutableList = mutableListOf()
+        layoutManager = LinearLayoutManager(context, LinearLayout.VERTICAL, false)
+        myAdapter = MainAdapter(mutableList!!)
+    }
+
+    private fun setupList() {
+        pastFormulaList!!.layoutManager = layoutManager
+        pastFormulaList!!.adapter = myAdapter
+    }
+
+//    private fun loadData() {
+//        for (i in 0..9) {
+//            val formula:String = "2+"+i
+//            val solution:Int = 2.0+i
+//            val myItem = Operation(OperationType.ADDITION, formula, solution)
+//            mutableList!!.add(myItem)
+//        }
+//        myAdapter!!.notifyDataSetChanged()
+//    }
+
+    private fun setUpCalculator() {
+        val operandA: Int = Random().nextInt(100 - 1) + 1;
+        val operandB: Int = Random().nextInt(100 - 1) + 1;
+
+
     }
 
 }
