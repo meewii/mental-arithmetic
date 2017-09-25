b16081a3facac9a5dcf4f8d06a31b4e89c6bc093
Marie-Anne Sinfreu <ma.sinfreu@gmail.com>
2017-09-25 15:50:24 +0200

    [wip] new navigation with listviews

---
+++ app/src/main/java/com/meewii/mentalarithmetic/ui/activities/NewGameActivity.kt
@@ -0,0 +1,15 @@
+package com.meewii.mentalarithmetic.ui.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.meewii.mentalarithmetic.R
+
+class NewGameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+
+
+    }
+}
